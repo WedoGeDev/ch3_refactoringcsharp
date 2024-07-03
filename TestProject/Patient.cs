43c503bd1887511b3d5e2f1c748627505e7b5328
Omar García Espíritu <oespiritu@crt.org.mx>
2024-07-03 11:40:05 -0600

    Writing a Test: Using Asserts

---
+++ TestProject/Patient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Refactoring_CaloriesCalculator
+{
+    public abstract class Patient
+    {
+        public string SSN { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double HeightInInches { get; set; }
+        public double WeightInPounds { get; set; }
+        public double Age { get; set; }
+
+        public abstract double DailyCaloriesRecommended();
+        public abstract double IdealBodyWeight();
+        
+        public double DistanceFromIdealWeight()
+        {
+            return WeightInPounds - IdealBodyWeight();
+        }
+    }
+}
---
+++ TestProject/TestFemalePatient.cs
@@ -0,0 +1,36 @@
+﻿using Refactoring_CaloriesCalculator;
+
+namespace TestProject;
+
+[TestFixture]
+public class TestFemalePatient
+{
+    [Test]
+    public void TestIdealBodyWeight()
+    {
+        var femalePatient = new FemalePatient();
+        femalePatient.HeightInInches = 72;
+        femalePatient.WeightInPounds = 110;
+        femalePatient.Age = 30;
+
+        double expectedResult = 161.15626;
+        double realResult = femalePatient.IdealBodyWeight();
+
+        Assert.AreEqual(expectedResult, realResult);
+    }
+
+    [Test]
+    public void TestDailyCaloriesRecomended()
+    {
+        var femalePatient = new FemalePatient();
+        femalePatient.HeightInInches = 72;
+        femalePatient.WeightInPounds = 110;
+        femalePatient.Age = 30;
+
+        double expectedResult = 1325.4;
+        double realResult = femalePatient.DailyCaloriesRecommended();
+
+        Assert.AreEqual(expectedResult, realResult);
+    }
+
+}
